6aa1c3b838b67c8a78580f462028a2ef5760f1cb
Jason Cook <the.root.user@gmail.com>
2019-06-25 14:12:26 -0400

    Import base work to start from

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "reset-css";
+import "normalize.css";
+import "./App.css";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      TodoData: [],
+      inputText: "Input a Todo",
+      newItem: "",
+      completed: false
+    };
+  }
+
+  componentDidMount = () => {
+    if (!localStorage.todoData) {
+      localStorage.setItem('todoData', JSON.stringify(this.state.TodoData))
+    }
+
+    let storedTodoArr = JSON.parse(localStorage.todoData)
+    this.setState({
+      TodoData: storedTodoArr
+    });
+  }
+  
+  componentDidUpdate() {
+    localStorage.setItem('todoData', JSON.stringify(this.state.TodoData))
+  }
+
+  addNewItem = event => {
+    event.preventDefault();
+    if (this.state.newItem !== "") {
+      this.setState(
+        prevState => {
+          return {
+            TodoData: [...prevState.TodoData, {
+              todo: this.state.newItem,
+              id: Date.now(),
+              completed: false
+            } ],
+            newItem: ""
+          };
+        },
+      );
+    }
+  };
+
+  formEventHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  clearHandler = event =>{
+    event.preventDefault();
+    
+    const clearedArray = [...this.state.TodoData].filter(cur => cur.completed === false)
+
+    this.setState({TodoData: clearedArray})
+  }
+
+
+  toggleHandler = event => {
+    const toggledArray = [...this.state.TodoData]
+    let position = null;
+
+    const target = toggledArray.find((cur, index) => {
+      position = index
+      return cur.id === Number.parseInt(event.target.getAttribute('data-key'), 10)
+    });
+
+    target.completed === false ? target.completed = true : target.completed=false;
+    
+    toggledArray[position] = target;
+
+    this.setState({TodoData: toggledArray});
+  };
+
+  render() {
+    return (
+      <div className="appContainer">
+        <h1>A Simple Todo List</h1>
+        <TodoForm
+          addNewItem={this.addNewItem}
+          inputText={this.state.inputText}
+          newItem={this.state.newItem}
+          formEventHandler={this.formEventHandler}
+          clearButton={this.clearHandler}
+        />
+        <TodoList todoData={this.state.TodoData} toggleItem={this.toggleHandler} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ todo/src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Todo.css";
+
+const TodoForm = props => {
+  return (
+    <div className="todoForm">
+      <form>
+        <input
+          type="text"
+          name="newItem"
+          onKeyPress={e => {
+            if (e.keyCode === 13) props.addNewItem();
+          }}
+          onChange={props.formEventHandler}
+          value={props.newItem}
+          // placeholder={props.inputText}
+          className="inputField"
+          id="todo"
+          required
+        />
+        <label>
+          <span>{props.inputText}</span>
+        </label>
+        <div className="formButtons">
+          <input
+            type="submit"
+            onClick={props.addNewItem}
+            value="Add To List"
+            className="subBtn"
+          />
+          <input
+            type="button"
+            onClick={props.clearButton}
+            value="Clear Complete"
+            className="clearBtn"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
